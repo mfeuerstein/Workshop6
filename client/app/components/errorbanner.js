a245063e409bfd92a65175413d21a01e01b60005
mfeuerstein <michael.a.feuerstein@gmail.com>
2017-04-13 14:50:42 -0400

    failbook

--- client/app/components/errorbanner.js
+++ client/app/components/errorbanner.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {hideElement} from '../util';
 
+// read the workshop
+
 export default class ErrorBanner extends React.Component {
   constructor(props) {
     super(props);
@@ -8,7 +10,7 @@ export default class ErrorBanner extends React.Component {
       active: false,
       errors: ""
     };
-    
+
     // ASSUMPTION: There is only one ErrorBanner component ever created.
     // By assigning to 'window', this is a global function. Global functions
     // are not typically a good idea, but they can be useful for adding basic
@@ -20,7 +22,7 @@ export default class ErrorBanner extends React.Component {
       })
     };
   }
-  
+
   render() {
     return (
       <div className={"alert alert-warning " + hideElement(!this.state.active)} role="alert">
